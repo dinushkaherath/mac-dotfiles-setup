67a1ebced4665948625644fedaf8927adbfa5269
dinushkaherath <dinushkaherath@gmail.com>
2021-08-10 09:49:59 -0400

    update midcap start tm

--- bin/midcap_tmux.sh
+++ bin/midcap_tmux.sh
@@ -3,13 +3,12 @@ if tmux has-session -t 'midcap' 2> /dev/null; then
     exit 0
 fi
 
-tmux new -s "midcap" -d \; split-window -h \; split-window -h \; select-layout even-horizontal \; split-window -v \; select-pane -t 0 \;
+tmux new -s "midcap" -d \; split-window -v \; select-pane -t 0 \;
 
 #Tail logs
-tmux send-keys -t midcap:0.2 'rails s' C-m
-tmux send-keys -t midcap:0.3 'cd hasura' C-m
-tmux send-keys -t midcap:0.3 'clear' C-m
-tmux send-keys -t midcap:0.3 './docker-run.sh' C-m
+tmux send-keys -t midcap:0.0 'rails s' C-m
+tmux send-keys -t midcap:0.1 'cd hasura' C-m
+tmux send-keys -t midcap:0.1 './docker-run.sh' C-m
 
 #attach to tmux midcap window
 tmux attach -t 'midcap'
