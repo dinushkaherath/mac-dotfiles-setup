6eab9ee9e13508d163c0615a71d95c43a09c37d0
dinushkaherath <dinushka.herath@gmail.com>
2021-11-25 13:20:25 -0500

    add master for gcom

--- bin/midcap_tmux.sh
+++ bin/midcap_tmux.sh
@@ -6,7 +6,9 @@ fi
 tmux new -s "midcap" -d \; split-window -v \; select-pane -t 0 \;
 
 #Tail logs
-tmux send-keys -t midcap:0.0 'rails s' C-m
+tmux send-keys -t midcap:0.0 'export MASTER="midcap-staging"' C-m
+tmux send-keys -t midcap:0.0 'rs' C-m
+tmux send-keys -t midcap:0.1 'export MASTER="midcap-staging"' C-m
 tmux send-keys -t midcap:0.1 'cd hasura' C-m
 tmux send-keys -t midcap:0.1 './docker-run.sh' C-m
 
