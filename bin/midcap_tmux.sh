644ed1c994aabdf3cd924472654768325a74a70a
dinushkaherath <dinushka.herath@gmail.com>
2022-01-12 15:54:30 -0500

    run hasura on top

--- bin/midcap_tmux.sh
+++ bin/midcap_tmux.sh
@@ -7,10 +7,9 @@ tmux new -s "midcap" -d \; split-window -v \; select-pane -t 0 \;
 
 #Tail logs
 tmux send-keys -t midcap:0.0 'export MASTER="midcap-staging"' C-m
-tmux send-keys -t midcap:0.0 'rs' C-m
+tmux send-keys -t midcap:0.0 'cd hasura' C-m
+tmux send-keys -t midcap:0.0 './docker-run.sh' C-m
 tmux send-keys -t midcap:0.1 'export MASTER="midcap-staging"' C-m
-tmux send-keys -t midcap:0.1 'cd hasura' C-m
-tmux send-keys -t midcap:0.1 './docker-run.sh' C-m
 
 #attach to tmux midcap window
 tmux attach -t 'midcap'
