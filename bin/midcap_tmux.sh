ade81c71d4a4c3e94d8b8412175f77bfdff298dd
dinushkaherath <dinushka.herath@gmail.com>
2022-04-30 20:57:51 -0400

    remove docker run for midcap

--- bin/midcap_tmux.sh
+++ bin/midcap_tmux.sh
@@ -7,8 +7,6 @@ tmux new -s "midcap" -d \; split-window -v \; select-pane -t 0 \;
 
 #Tail logs
 tmux send-keys -t midcap:0.0 'export MASTER="midcap-staging"' C-m
-tmux send-keys -t midcap:0.0 'cd hasura' C-m
-tmux send-keys -t midcap:0.0 './docker-run.sh' C-m
 tmux send-keys -t midcap:0.1 'export MASTER="midcap-staging"' C-m
 
 #attach to tmux midcap window
